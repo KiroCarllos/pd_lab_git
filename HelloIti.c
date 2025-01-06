2d96a8f0837072ffa6810f7d5b169da6a6049ed5
AsmaaMohamed76 <113949635+AsmaaMohamed76@users.noreply.github.com>
2025-01-06 13:10:48 +0200

    update methods

--- HelloIti.c
+++ HelloIti.c
@@ -1,20 +1,16 @@
 class HelloIti
 {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
     void print()
     {
     }
-	void printHelloIti()
-=======
-	void printHelloIti():base("app")
->>>>>>> fca5f8e29bdd46772cb1aff40bfc04e0b5f777c0
-=======
-	void printHelloIti() : base("app")
 
-							   void printHelloIti()
->>>>>>> 7ad54b9ab848e79ae7db1f44ba25dcc2f8cc2aa0
+	void printHelloIti():base("app")
+
+        void printHelloIti()
+
 	{
 		printf("Hello From Class Iti");
 	}
